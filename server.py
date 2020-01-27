3cc830c26501ed0c966a82fee9833c440d8cb348
Greg Leonard <gr3gl@hotmail.com>
2020-01-27 17:57:25 +0000

    refactoring

--- server.py
+++ server.py
@@ -3,12 +3,6 @@ from flask import render_template, redirect
 from rpi_rf import RFDevice
 app = Flask(__name__)
 
-@app.route("/")
-def main_page():
-    return render_template("main.html")
-
-@app.route("/allon")
-def all_on():
 TRANSMIT_PIN = 2
 ON_MAIN = 70741
 ON_SEC = 83029
@@ -17,11 +11,16 @@ OFF_SEC = 83028
 TRANS_LENGTH = 350
 PROTOCOL = 1
 
-trans = RFDevice(TRANSMIT_PIN)
-trans.enable_tx()
-trans.tx_repeat = 10
 
+@app.route("/")
+def main_page():
+    return render_template("main.html")
 
+@app.route("/allon")
+def all_on():
+    trans = RFDevice(TRANSMIT_PIN)
+    trans.enable_tx()
+    trans.tx_repeat = 10
     trans.tx_code(ON_MAIN, PROTOCOL, TRANS_LENGTH, 24)
     trans.tx_code(ON_SEC, PROTOCOL, TRANS_LENGTH, 24)
     trans.cleanup()
@@ -29,6 +28,9 @@ trans.tx_repeat = 10
 
 @app.route("/alloff")
 def all_off():
+    trans = RFDevice(TRANSMIT_PIN)
+    trans.enable_tx()
+    trans.tx_repeat = 10
     trans.tx_code(OFF_MAIN, PROTOCOL, TRANS_LENGTH, 24)
     trans.tx_code(OFF_SEC, PROTOCOL, TRANS_LENGTH, 24) 
     trans.cleanup()
@@ -36,29 +38,41 @@ def all_off():
 
 @app.route("/mainon")
 def main_on():
+    trans = RFDevice(TRANSMIT_PIN)
+    trans.enable_tx()
+    trans.tx_repeat = 10
     trans.tx_code(ON_MAIN, PROTOCOL, TRANS_LENGTH, 24)
     trans.cleanup()
     return redirect("/")
 
 @app.route("/mainoff")
 def main_off():
+    trans = RFDevice(TRANSMIT_PIN)
+    trans.enable_tx()
+    trans.tx_repeat = 10
     trans.tx_code(OFF_MAIN, PROTOCOL, TRANS_LENGTH, 24)
     trans.cleanup()
     return redirect("/")
 
 @app.route("/secon")
 def sec_on():
+    trans = RFDevice(TRANSMIT_PIN)
+    trans.enable_tx()
+    trans.tx_repeat = 10
     trans.tx_code(ON_SEC, PROTOCOL, TRANS_LENGTH, 24)
     trans.cleanup()
     return redirect("/")
 
 @app.route("/secoff")
 def sec_off():
+    trans = RFDevice(TRANSMIT_PIN)
+    trans.enable_tx()
+    trans.tx_repeat = 10
     trans.tx_code(OFF_SEC, PROTOCOL, TRANS_LENGTH, 24)
     trans.cleanup()
     return redirect("/")
     
 
 if __name__ == "__main__":
+    app.run(host="0.0.0.0")
 
-app.run(host="0.0.0.0")
