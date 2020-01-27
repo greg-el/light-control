1bf2d84b826c08d2e12a2037b70ed064e6b85ac9
greg-el <gr3gl@hotmail.com>
2020-01-27 17:54:12 +0000

    refactoring

--- server.py
+++ server.py
@@ -3,6 +3,12 @@ from flask import render_template, redirect
 from rpi_rf import RFDevice
 app = Flask(__name__)
 
+@app.route("/")
+def main_page():
+    return render_template("main.html")
+
+@app.route("/allon")
+def all_on():
 TRANSMIT_PIN = 2
 ON_MAIN = 70741
 ON_SEC = 83029
@@ -15,42 +21,44 @@ trans = RFDevice(TRANSMIT_PIN)
 trans.enable_tx()
 trans.tx_repeat = 10
 
-@app.route("/")
-def main_page():
-    return render_template("main.html")
 
-@app.route("/allon")
-def all_on():
     trans.tx_code(ON_MAIN, PROTOCOL, TRANS_LENGTH, 24)
     trans.tx_code(ON_SEC, PROTOCOL, TRANS_LENGTH, 24)
+    trans.cleanup()
     return redirect("/")
 
 @app.route("/alloff")
 def all_off():
     trans.tx_code(OFF_MAIN, PROTOCOL, TRANS_LENGTH, 24)
     trans.tx_code(OFF_SEC, PROTOCOL, TRANS_LENGTH, 24) 
+    trans.cleanup()
     return redirect("/")
 
 @app.route("/mainon")
 def main_on():
     trans.tx_code(ON_MAIN, PROTOCOL, TRANS_LENGTH, 24)
+    trans.cleanup()
     return redirect("/")
 
 @app.route("/mainoff")
 def main_off():
     trans.tx_code(OFF_MAIN, PROTOCOL, TRANS_LENGTH, 24)
+    trans.cleanup()
     return redirect("/")
 
 @app.route("/secon")
 def sec_on():
     trans.tx_code(ON_SEC, PROTOCOL, TRANS_LENGTH, 24)
+    trans.cleanup()
     return redirect("/")
 
 @app.route("/secoff")
 def sec_off():
     trans.tx_code(OFF_SEC, PROTOCOL, TRANS_LENGTH, 24)
+    trans.cleanup()
     return redirect("/")
     
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+
+app.run(host="0.0.0.0")
